b1d1c84b63fc6d3640608201d0ff66d442db42aa
Æsir Dev <dev@aesirdev.com>
2025-02-26 15:54:32 +0100

    moving a lot of stuff to devenv

--- devenv.nix
+++ devenv.nix
@@ -1,71 +1,150 @@
-{ pkgs, ... }:
+{ inputs, lib, pkgs, config, ... }:
+
+let
+  unstable = import inputs.nixpkgs-unstable { system = pkgs.stdenv.system; };
+in
+
 {
+  cachix.enable = true;
+  cachix.pull = [ "pre-commit-hooks" ];
+
   env.TAURI_APP_PATH = "apps/tauri/";
   env.TAURI_FRONTEND_PATH = "apps/ui/";
 
   languages.javascript.enable = true;
   languages.javascript.bun.enable = true;
-  languages.javascript.bun.package = pkgs.unstable.bun;
+  languages.javascript.bun.package = unstable.bun;
+
   languages.rust.enable = true;
-  languages.rust.channel = "beta";
+  languages.rust.channel = "stable";
+  languages.rust.components = [ "rustc" "cargo" "clippy" "rustfmt" "rust-analyzer" "llvm-tools-preview" ];
 
   packages = with pkgs; [ cargo-expand cargo-tauri sea-orm-cli ];
 
-  scripts.tauri.exec = ''
-    (
-      # change working directory to git toplevel
-      cd "$(${pkgs.lib.getExe pkgs.git} rev-parse --show-toplevel)"
-      # execute command from here
-      ${pkgs.lib.getExe pkgs.cargo-tauri} "$@"
-    )
+  git-hooks.hooks = {
+    cargo-check.enable = true;
+    check-added-large-files.enable = true;
+    check-case-conflicts.enable = true;
+    clippy.enable = true;
+    clippy.settings.denyWarnings = true;
+    rustfmt.enable = true;
+    rustfmt.settings.all = true;
+    rustfmt.settings.check = true;
+  };
+
+  scripts = {
+    workdir.exec = ''
+      # get working directory
+      WORK_DIR="$(git rev-parse --show-superproject-working-tree 2>/dev/null)"
+      if [ -z "$WORK_DIR" ]; then
+        WORK_DIR="$(git rev-parse --show-toplevel)"
+      fi
+      echo "$WORK_DIR"
+    '';
+
+    nuxi.exec = ''
+      (
+        cd "$(workdir)/apps/ui" || { echo "Failed to cd to $(workdir)/apps/ui"; exit 1; }
+        ${lib.getExe config.languages.javascript.bun.package} x nuxi "$@"
+      )
+    '';
+
+    tauri.exec = ''
+      (
+        cd "$(workdir)" || { echo "Failed to cd to $(workdir)"; exit 1; }
+        ${lib.getExe pkgs.cargo-tauri} "$@" || { echo "Failed to excute 'cargo-tauri $*'"; exit 1; }
+      )
+    '';
+
+    sea-orm-cli.exec = ''
+      (
+        cd "$(workdir)" || { echo "Failed to cd to $(workdir)"; exit 1; }
+        ${lib.getExe' pkgs.sea-orm-cli "sea-orm-cli"} "$@"
+      )
+    '';
+  };
+
+  tasks = {
+    "photos:dbUrl" = {
+      exec = ''
+        app_id="$(jq -r .identifier < "$(workdir)/apps/tauri/tauri.conf.json")"
+        db_name="photos.db"
+
+        case "$(uname -s)" in
+          Darwin*)
+            db_path="$HOME/Library/Application Support/$app_id/$db_name"
+            ;;
+          Linux*)
+            db_path="$HOME/.local/share/$app_id/$db_name"
+            ;;
+          MINGW*|CYGWIN*|MSYS*)
+            db_path="$APPDATA\\$app_id\\$db_name"
+            ;;
+        esac
+
+        export DATABASE_URL="sqlite://$db_path?mode=rwc"
+      '';
+      exports = [ "DATABASE_URL" ];
+      before = [ "devenv:enterShell" ];
+    };
+
+    "photos:coverage" = {
+      exec = ''
+        (
+          cd "$WORK_DIR" || { echo "Failed to cd to $WORK_DIR"; exit 1; }
+          [ -d "./coverage" ] && rm -r "./coverage"
+          mkdir ./coverage
+
+          echo "=== running coverage ==="
+          CARGO_INCREMENTAL=0 \
+            RUSTFLAGS="-Cinstrument-coverage" \
+            LLVM_PROFILE_FILE="cargo-test-%p-%m.profraw" \
+            ${lib.getExe' config.languages.rust.toolchain.cargo "cargo"} test
+          echo "ok."
+
+          echo "=== generating report ==="
+          fmt="html"
+          file="coverage/html"
+          if ${if config.devenv.isTesting then "true" else "false"}; then
+            fmt="lcov"
+            file="coverage/tests.lcov"
+          fi
+          ${lib.getExe pkgs.grcov} . \
+            --binary-path="./target/debug/deps" \
+            -s . \
+            -t "$fmt" \
+            --branch \
+            --ignore-not-existing \
+            --ignore "../*" \
+            --ignore "/*" \
+            --ignore "*/tests/*" \
+            --ignore "target/*" \
+            -o "$file"
+          echo "ok."
+
+          echo "=== cleaning up ==="
+          find -name "*.profraw" -exec rm "{}" \;
+          echo "ok."
+
+          if ${if config.devenv.isTesting then "false" else "true"}; then
+            open "coverage/html/index.html" 
+          fi
+
+          echo "report location: $file"
+        )
+      '';
+
+      after = [ "devenv:enterTest" ];
+    };
+  };
+
+  test = pkgs.writeShellScript "devenv-test" ''
+    # FIX: we don't want it to run `enterShell` twice
+    # echo "• Setting up shell environment ..."
+    # ${config.enterShell}
+
+    set -euo pipefail
+    echo "• Testing ..."
+    ${config.enterTest}
   '';
-
-  scripts.nuxi.exec = ''
-    (
-      # change working directory to git toplevel/apps/ui
-      cd "$(${pkgs.lib.getExe pkgs.git} rev-parse --show-toplevel)/apps/ui"
-      # execute command from here
-      ${pkgs.lib.getExe pkgs.unstable.bun} x nuxi "$@"
-    )
-  '';
-
-  scripts."sea-orm-cli".exec = ''
-    ROOT="$(${pkgs.lib.getExe pkgs.git} rev-parse --show-toplevel)/apps/tauri"
-    APP_ID=$(cat "$ROOT/tauri.conf.json" | ${pkgs.lib.getExe pkgs.jq} -r .identifier)
-    DB_NAME=photos.db
-
-    case "$(uname -s)" in
-      Darwin*)
-        DB_PATH="$HOME/Library/Application Support/$APP_ID/$DB_NAME"
-        ;;
-      Linux*)
-        DB_PATH="$HOME/.local/share/$APP_ID/$DB_NAME"
-        ;;
-      MINGW*|CYGWIN*|MSYS*)
-        DB_PATH="$APPDATA\\$APP_ID\\$DB_NAME"
-        ;;
-    esac
-
-    DATABASE_URL="sqlite://$DB_PATH?mode=rwc" ${pkgs.lib.getExe pkgs.sea-orm-cli} "$@"
-  '';
-
-  scripts.update-submodules.exec = ''
-    (
-      # change working directory to git toplevel
-      cd "$(${pkgs.lib.getExe pkgs.git} rev-parse --show-toplevel)"
-      # update and apply patch to xtaskops
-      ${pkgs.lib.getExe pkgs.git} submodule update --remote vendors/xtaskops
-      cd "vendors/xtaskops"
-      ${pkgs.lib.getExe pkgs.git} apply ../patches/xtaskops_ci_args.patch
-    )
-  '';
-
-  # TODO: add a script to generate code coverage
-  # scripts.coverage.exec = ''
-  #   (
-  #     # change working directory to git toplevel
-  #     cd "$(${pkgs.lib.getExe pkgs.git} rev-parse --show-toplevel)"
-  #     docker run --rm -itv "$(pwd):/volume" xd009642/tarpaulin sh -c 'cargo tarpaulin "$@"'
-  #   )
-  # '';
 }
